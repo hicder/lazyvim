e3c96b9efd29330275a6dead0f1686b1791f4477
Hieu Pham <hpham@roblox.com>
2025-07-24 17:01:16 -0700

    Add kimi-k2 to avante

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -6,11 +6,12 @@ return {
     event = "VeryLazy",
     version = false, -- Never set this value to "*"! Never!
     opts = {
-      provider = "gemini_flash",
+      provider = "kimi_k2_groq",
       providers = {
         gemini = {
           model = "gemini-2.5-pro",
           timeout = 30000,
+          use_ReAct_prompt = true,
           extra_request_body = {
             generation_config = {
               temperature = 0,
@@ -18,15 +19,35 @@ return {
           },
         },
         gemini_flash = {
-         __inherited_from = "gemini",
+          __inherited_from = "gemini",
           model = "gemini-2.5-flash",
           timeout = 30000,
+          use_ReAct_prompt = true,
           extra_request_body = {
             generationConfig = {
               temperature = 0,
             },
           },
-        }
+        },
+        moonshot = {
+          endpoint = "https://api.moonshot.ai/v1",
+          model = "kimi-k2-0711-preview",
+          timeout = 30000, -- Timeout in milliseconds
+          extra_request_body = {
+            temperature = 0.75,
+            max_tokens = 32768,
+          },
+        },
+        kimi_k2_groq = {
+          __inherited_from = "openai",
+          endpoint = "https://api.groq.com/openai/v1",
+          model = "moonshotai/kimi-k2-instruct",
+          api_key_name = "AVANTE_GROQ_API_KEY",
+          extra_request_body = {
+            temperature = 0.6,
+            max_tokens = 16384,
+          },
+        },
       },
       behaviour = {
         auto_suggestions = false,
@@ -68,12 +89,12 @@ return {
       },
       {
         -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
+        "MeanderingProgrammer/render-markdown.nvim",
         opts = {
           file_types = { "markdown", "Avante" },
         },
         ft = { "markdown", "Avante" },
       },
     },
-  }
+  },
 }
